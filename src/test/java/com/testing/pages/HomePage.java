b459c50e37a2d0187a2db15fb3740589d4c54e01
Jorge Luis Delgado <jorgedelgadopernia@gmail.com>
2022-06-24 20:05:59 -0300

    Refactoring and adding XML suite

--- src/test/java/com/testing/pages/HomePage.java
+++ src/test/java/com/testing/pages/HomePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +14,14 @@ import java.util.List;
 @Component
 public class HomePage extends BasePage {
 
+    //region Constants
+    private final String ITEM_LIST_XPATH = "//body/div[@id='content']/div[1]/div[1]/ul[1]/li";
+    private final String DELETE_ITEM_BUTTON = ".//div[1]/div[1]/div[1]/button[2]";
+    private final String EDIT_ITEM_BUTTON = ".//div[1]/div[1]/div[1]/button[1]";
+    private final String MODAL_DELETE_BUTTON = "//button[contains(text(),'Yes, delete it!')]";
+    //endregion
+
+    //region WebElements
     @FindBy(id="inputImage")
     private WebElement imageInput;
     @FindBy(tagName="textarea")
@@ -27,22 +34,25 @@ public class HomePage extends BasePage {
     private WebElement modalBox;
     @FindBy(xpath = "//button[contains(text(),'Update Item')]")
     private WebElement updateItemButton;
+    //endregion
 
-    private final String ITEM_LIST_XPATH = "//body/div[@id='content']/div[1]/div[1]/ul[1]/li";
-    private final String DELETE_ITEM_BUTTON = ".//div[1]/div[1]/div[1]/button[2]";
-    private final String EDIT_ITEM_BUTTON = ".//div[1]/div[1]/div[1]/button[1]";
-    private final String MODAL_DELETE_BUTTON = "//button[contains(text(),'Yes, delete it!')]";
-
+    //region Private Variables
     private int itemCount;
+    //endregion
 
+    //region Getters and Setters
     public int getItemCount(){ return itemCount; }
     public void setItemCount(int itemCount){ this.itemCount = itemCount; }
+    //endregion
 
+    //region Constructors
     @Autowired
     public HomePage(WebDriver driver) {
         super(driver);
     }
+    //endregion
 
+    //region Void Methods
     public void uploadImage(String filePath){
         wait.until(ExpectedConditions.elementToBeClickable(imageInput));
         imageInput.sendKeys(filePath);
@@ -55,22 +65,9 @@ public class HomePage extends BasePage {
         waitClickableAndClick(createItemButton);
     }
 
-    public String getImageSrcFromLastItem(){
-        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
-                By.xpath(ITEM_LIST_XPATH), itemCount));
-        return getLastItem().findElement(By.tagName("img")).getAttribute("src");
-    }
-
-    public String getTextFromLastItem(){
-        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
-                By.xpath(ITEM_LIST_XPATH), itemCount));
-        return getLastItem().findElement(By.tagName("p")).getText();
-    }
-
     public void deleteLastElementFromList(){
         waitClickableAndClick(getLastItem().findElement(By.xpath(DELETE_ITEM_BUTTON)));
-        wait.until(ExpectedConditions.visibilityOf(modalBox));
-        modalBox.findElement(By.xpath(MODAL_DELETE_BUTTON)).click();
+        waitClickableAndClick(modalBox.findElement(By.xpath(MODAL_DELETE_BUTTON)));
         wait.until(ExpectedConditions.numberOfElementsToBe(
                 By.xpath(ITEM_LIST_XPATH), itemCount));
     }
@@ -78,17 +75,6 @@ public class HomePage extends BasePage {
     public int getNewNumberOfItems(){
         wait.until(ExpectedConditions.visibilityOfAllElements(listOfItems));
         return listOfItems.size();
-    }
-
-    public boolean isButtonActive(){ return createItemButton.isEnabled(); }
-
-    public WebElement searchItemInListByText(){
-        for (int i=0; i < getNewNumberOfItems(); i++){
-            String itemText = listOfItems.get(i).findElement(By.tagName("p")).getText();
-            if (itemText.contains("Creators: Matt Duffer, Ross Duffer"))
-                return listOfItems.get(i);
-        }
-        return null;
     }
 
     public void editElementFromList(int index){
@@ -104,9 +90,38 @@ public class HomePage extends BasePage {
     public void clickUpdateDescriptionButton(){
         waitClickableAndClick(updateItemButton);
     }
+    //endregion
+
+    //region String Methods
+    public String getImageSrcFromLastItem(){
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
+                By.xpath(ITEM_LIST_XPATH), itemCount));
+        return getLastItem().findElement(By.tagName("img")).getAttribute("src");
+    }
+
+    public String getTextFromLastItem(){
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
+                By.xpath(ITEM_LIST_XPATH), itemCount));
+        return getLastItem().findElement(By.tagName("p")).getText();
+    }
 
     public String getTextFromItem(int index){
         return getItem(index).findElement(By.tagName("p")).getText();
+    }
+    //endregion
+
+    //region boolean Methods
+    public boolean isButtonActive(){ return createItemButton.isEnabled(); }
+    //endregion
+
+    //region WebElements Methods
+    public WebElement searchItemInListByText(String searchCriteria){
+        for (int i=0; i < getNewNumberOfItems(); i++){
+            String itemText = listOfItems.get(i).findElement(By.tagName("p")).getText();
+            if (itemText.contains(searchCriteria))
+                return listOfItems.get(i);
+        }
+        return null;
     }
 
     private WebElement getLastItem(){
@@ -118,4 +133,5 @@ public class HomePage extends BasePage {
         wait.until(ExpectedConditions.visibilityOfAllElements(listOfItems));
         return listOfItems.get(index);
     }
+    //endregion
 }
--- src/test/java/com/testing/tests/ItemCreationTest.java
+++ src/test/java/com/testing/tests/ItemCreationTest.java
@@ -7,6 +7,7 @@ import com.testing.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(classes = {Driver.class})
@@ -16,13 +17,14 @@ public class ItemCreationTest extends BaseTest {
     private HomePage homePage;
 
     @Test(description = "Create new item using valid data")
-    public void create_new_item_with_valid_data(){
+    @Parameters({"textDescription", "imagePath"})
+    public void create_new_item_with_valid_data(String textDescription, String imagePath){
         homePage.navigateTo("http://immense-hollows-74271.herokuapp.com/");
-        homePage.uploadImage("C:/Users/jorge/OneDrive/Documentos/Test.jpg");
-        homePage.fillDescription("TEST");
+        homePage.uploadImage(System.getProperty("user.dir") + imagePath);
+        homePage.fillDescription(textDescription);
         homePage.clickCreateItemButton();
-        Assert.assertTrue(homePage.getImageSrcFromLastItem().contains(Utils.getFileNameFromPath("C:/Users/jorge/OneDrive/Documentos/Test.jpg")));
-        Assert.assertTrue(homePage.getTextFromLastItem().contains("TEST"));
+        Assert.assertTrue(homePage.getImageSrcFromLastItem().contains(Utils.getFileNameFromPath(imagePath)));
+        Assert.assertTrue(homePage.getTextFromLastItem().contains(textDescription));
     }
 
     @Test(dependsOnMethods = "create_new_item_with_valid_data")
@@ -32,22 +34,25 @@ public class ItemCreationTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = "delete_newly_created_item")
-    public void create_item_with_more_than_300_characters_description(){
-        homePage.uploadImage("C:/Users/jorge/OneDrive/Documentos/Test.jpg");
-        homePage.fillDescription("tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam non nisi est sit amet facilisis magna etiam tempor orci eu lobortis elementum nibh tellus molestie nunc non blandit massa enim nec dui nunc mattis enim ut tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet");
+    @Parameters({"imagePath", "maxTextDescription"})
+    public void create_item_with_more_than_max_characters_description(String imagePath, String maxTextDescription){
+        homePage.uploadImage(System.getProperty("user.dir") + imagePath);
+        homePage.fillDescription(Utils.getRandomText(Integer.parseInt(maxTextDescription) + 1));
         Assert.assertFalse(homePage.isButtonActive());
     }
 
-    @Test(dependsOnMethods = "create_item_with_more_than_300_characters_description")
-    public void search_for_existing_item_in_list(){
-        Assert.assertNotNull(homePage.searchItemInListByText());
+    @Test(dependsOnMethods = "create_item_with_more_than_max_characters_description")
+    @Parameters({"searchCriteria"})
+    public void search_for_existing_item_in_list(String searchCriteria){
+        Assert.assertNotNull(homePage.searchItemInListByText(searchCriteria));
     }
 
     @Test(dependsOnMethods = "search_for_existing_item_in_list")
-    public void edit_an_existing_item(){
-        homePage.editElementFromList(4);
-        homePage.updateDescription("ANOTHER TEST");
+    @Parameters({"textDescription", "itemIndex"})
+    public void edit_an_existing_item(String textDescription, String itemIndex){
+        homePage.editElementFromList(Integer.parseInt(itemIndex));
+        homePage.updateDescription(textDescription);
         homePage.clickUpdateDescriptionButton();
-        Assert.assertTrue(homePage.getTextFromItem(4).contains("ANOTHER TEST"));
+        Assert.assertTrue(homePage.getTextFromItem(4).contains(textDescription));
     }
 }
